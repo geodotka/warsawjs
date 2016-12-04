380a47c494e7a1475ea39c7dc21c1a2d5ba12856
geodotka <dorota.kowalik@21s.pl>
2016-12-04 15:21:24 +0100

    Add arrow with links

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,64 @@
 var $imgList = document.querySelectorAll('.image');
-var $zoomContainer = document.querySelector('.zoom-container');
+var $imageContainer = document.querySelector('.image-container');
+var imgArray = Array.from($imgList);
+var currentIndex = 0;
 
 
-Array.from($imgList).forEach(function ($image) {
+imgArray.forEach(function ($image) {
     $image.addEventListener('click', function (ev) {
         ev.preventDefault();
-        var $imgWithBorder = document.querySelector('.img-with-border');
-        if ($imgWithBorder) {
-            $imgWithBorder.className = 'image';
-        }
-        $image.className = 'img-with-border';
-        var newImg = document.createElement('img');
-        newImg.src = $image.parentNode.href;
-        $zoomContainer.innerHTML = '';
-        $zoomContainer.appendChild(newImg)
+        previewImage($image);
     })
+});
+
+
+function previewImage($image) {
+    currentIndex = imgArray.indexOf($image);
+    setImageBorder($image);
+    createImageWidget($image);
+}
+
+
+function setImageBorder($image){
+    var $imgWithBorder = document.querySelector('.img-with-border');
+    if ($imgWithBorder) {
+        $imgWithBorder.className = 'image';
+    }
+    $image.className = 'img-with-border';
+}
+
+
+function createImageWidget($image){
+    $imageContainer.innerHTML = '';
+    var newImg = document.createElement('img');
+    newImg.src = $image.parentNode.href;
+    $imageContainer.appendChild(newImg);
+}
+
+
+function getPrevImage() {
+    if (currentIndex == 0){
+        return imgArray[imgArray.length - 1]
+    }
+    return imgArray[--currentIndex]
+}
+
+
+function getNextImage() {
+    if (currentIndex == imgArray.length - 1){
+        return imgArray[0]
+    }
+    return imgArray[++currentIndex]
+}
+
+
+document.querySelector('.arrow-left').addEventListener('click', function(){
+    var $prevImage = getPrevImage();
+    previewImage($prevImage)
+});
+
+
+document.querySelector('.arrow-right').addEventListener('click', function(){
+    var $nextImage = getNextImage();
+    previewImage($nextImage)
 });

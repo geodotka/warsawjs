b4c944c09a8411509207467c59346513161b1611
geodotka <dorota.kowalik@21s.pl>
2016-12-04 12:58:47 +0100

    Show gallery with preview

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,18 @@
-/**
- * Created by dorota on 04.12.16.
- */
+var $imgList = document.querySelectorAll('.image');
+var $zoomContainer = document.querySelector('.zoom-container');
+
+
+Array.from($imgList).forEach(function ($image) {
+    $image.addEventListener('click', function (ev) {
+        ev.preventDefault();
+        var $imgWithBorder = document.querySelector('.img-with-border');
+        if ($imgWithBorder) {
+            $imgWithBorder.className = 'image';
+        }
+        $image.className = 'img-with-border';
+        var newImg = document.createElement('img');
+        newImg.src = $image.parentNode.href;
+        $zoomContainer.innerHTML = '';
+        $zoomContainer.appendChild(newImg)
+    })
+});
